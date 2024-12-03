0d90dc419a20993a22b5d7a8a05c3c1a44076f3d
andremauro17 <139780767+andremauro17@users.noreply.github.com>
2024-12-03 12:18:48 +0100

    Aggiunto controllo Factory

--- Codice/contracts/Factory.sol
+++ Codice/contracts/Factory.sol
@@ -8,6 +8,7 @@ contract CampaignFactory {
     address[] public campaigns;
 
     function createCampaign(string memory nameC, address crowdsourcer,int256 _lat,int256 _long,int256 _tol,string memory camptype,int256 numProd) public {
+        require(msg.sender == crowdsourcer,"The addresses aren't the same");
         Campaign newCampaign = new Campaign(nameC, crowdsourcer,_lat,_long,_tol,camptype,numProd);
         campaigns.push(address(newCampaign));
     }
